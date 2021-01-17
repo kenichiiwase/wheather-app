c7e3876e81cdbc6da268ce1c749490afb2562d2c
kenichiiwase <44935028+kenichiiwase@users.noreply.github.com>
2021-01-17 05:07:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,50 +3,43 @@ const nodemailer = require("nodemailer");
 const cron = require("node-cron");
 require("date-utils");
 
-//api
+// api
 const appid = "APIキー";
 
 async function callweather() {
   const res = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${appid}&lang=ja&units=metric`
+    `http://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${appid}&lang=ja&units=metric`,
   );
   const wresult = await res.json();
   return wresult;
 }
 
 async function weatherMailSend() {
-  //天気情報取得
+  // 天気情報取得
   const weatherobj = await callweather();
-  const weather = weatherobj.weather;
-  const weatherresult = weather[0].description;
+  const weatherresult = weatherobj.weather[0].description;
 
-  //現在日時を取得
-  let nowdate = new Date().toFormat("YYYY年MM月DD日");
+  // 現在日時を取得
+  const nowdate = new Date().toFormat('YYYY年MM月DD日');
 
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    service: 'Gmail',
     auth: {
-      user: "Gmailアドレス",
-      pass: "Gmailパスワード",
+      user: 'gmailメール',
+      pass: 'パスワード',
     },
   });
 
   // メールの内容
   const mailOptions1 = {
-    to: "送信するメールアドレス",
+    to: 'gmailメールアドレス',
     subject: `${nowdate} :本日のお天気`,
-    text: `本日の天気は、${weatherresult} 'です。`,
+    text: `本日の天気は、${weatherresult} です。`,
   };
-  try {
-    // 送信
-    const result = await transporter.sendMail(mailOptions1);
-    console.log(result);
-  } catch (e) {
-    console.error(e);
-  }
+  // 送信
+  await transporter.sendMail(mailOptions1);
 }
-
-//朝9時に天気情報メールを送信
+// 朝9時に天気情報メールを送信
 cron.schedule('0 0 9 * * *', () => {
     weatherMailSend();
 },{
